3065b84a3253cb9faa3936078868e7f21fb9b743
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-05-27 11:14:34 +0530

    added final

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import BackButton from './BackButton';
-
-
+import React from "react";
+import BackButton from "./BackButton";
+import "./finalScreen.css"
 const FinalScreen = ({ bananas, onRestart, onBack }) => {
   return (
     <div className="final-screen">
       <BackButton onBack={onBack} />
-      <h1>Game Over</h1>
+      {/* <h1>Game Over</h1>
       <p>Total Matches: {bananas}</p>
-      <button onClick={onRestart}>Restart</button>
+      <button onClick={onRestart}>Restart</button> */}
+      <img src="final.png" alt="final" className="final" />
     </div>
   );
 };
